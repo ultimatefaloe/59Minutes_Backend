e26e977eaee502f34b4cad7ae599e8d8bd528206
ultimatefaloe <ultimatefaloe@outlook.com>
2025-04-21 22:20:47 +0100

    mongodb connection

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ export default {
     },
 
     mongo: {
-        encodedusername: process.env.DB_USERNAME,
-        encodedpassword: process.env.DB_PASSWORD
+        encodedusername: process.env.DB_USERNAME || `troy19156@gmail.com`,
+        encodedpassword: process.env.DB_PASSWORD || `Ultimate@25`
     }
 }
